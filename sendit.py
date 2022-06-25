5a3220dd9127944dcd272bd7a3716b9718cfec89
Salty-Coder <59771980+Salty-Coder@users.noreply.github.com>
2022-06-25 19:28:28 +0100

    Fix reading if question in file

--- sendit.py
+++ sendit.py
@@ -5,11 +5,12 @@ from colorama import Fore, Style, init
 
 init(convert=True)
 
-file = open("responses.txt", "a+", encoding='utf-8')
+file = open("responses.txt", "a", encoding='utf-8')
+fileR = open("responses.txt", encoding='utf-8')
 
 
 times = int(input("Times to test: "))
-cooldown = float(input("Cooldown: "))
+cooldown = float(input("Cooldown: ") or 0)
 
 if times < 1:
     print("Times must be greater than 0")
@@ -21,7 +22,7 @@ else:
 
         question = y["question"]
 
-        if question in file:
+        if question in fileR.read():
             print(f"{Fore.RED}Question found in list, skipping...{Style.RESET_ALL}")
         else:
             print(f"{Fore.GREEN}Question not found in list, adding to list...{Style.RESET_ALL}")
